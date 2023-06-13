c01da84205f114efa27716964e974ba7cdc372aa
Crhisanto76 <antonypoure2@gmail.com>
2023-06-13 16:02:34 +0200

    import files

---
+++ app.js
@@ -0,0 +1,45 @@
+const loader = document.querySelector(".loader-container");
+const errorInformation = document.querySelector(".error-information");
+
+async function getWeatherData(){
+  try {
+    const response = await fetch(
+      "http://api.airvisual.com/v2/nearest_city?key=db5d9e1c-51c7-410b-be99-f5f0bc0d720c"
+    )
+
+    if(!response.ok) {
+      throw new Error(`Error ${response.status}, ${response.statusText}`)
+    }
+
+    const responseData = await response.json();
+    
+    const sortedData = {
+      city: responseData.data.city,
+      country: responseData.data.country,
+      iconId: responseData.data.current.weather.ic,
+      temperature: responseData.data.current.weather.tp,
+    }
+
+    populateUI(sortedData)
+  }
+  catch (error) {
+    loader.classList.remove("active");
+    errorInformation.textContent = error.message;
+  }
+}
+getWeatherData()
+
+
+const cityName = document.querySelector(".city-name");
+const countryName = document.querySelector(".country-name");
+const temperature = document.querySelector(".temperature");
+const infoIcon = document.querySelector(".info-icon");
+
+function populateUI(data){
+  cityName.textContent = data.city;
+  countryName.textContent = data.country;
+  temperature.textContent = `${data.temperature}°`;
+  infoIcon.src = `ressources/icons/${data.iconId}.svg`;
+  infoIcon.style.width = "150px";
+  loader.classList.remove("active");
+}
