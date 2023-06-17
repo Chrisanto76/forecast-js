14457dabcfa06ecf4502d2c1af7147c9e1b33b55
Chrisanto76 <antonypoure2@gmail.com>
2023-06-17 13:41:30 +0200

    add css grid and weather api

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-const loader = document.querySelector(".loader-container");
-const errorInformation = document.querySelector(".error-information");
+const loader = document.querySelector('.loader-container');
+const errorInformation = document.querySelector('.error-information');
 
-async function getWeatherData(){
-  try {
-    const response = await fetch(
-      "http://api.airvisual.com/v2/nearest_city?key=db5d9e1c-51c7-410b-be99-f5f0bc0d720c"
-    )
+async function getWeatherData() {
+    try {
+        const response = await fetch(
+            'http://api.airvisual.com/v2/nearest_city?key=db5d9e1c-51c7-410b-be99-f5f0bc0d720c'
+        );
 
-    if(!response.ok) {
-      throw new Error(`Error ${response.status}, ${response.statusText}`)
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}, ${response.statusText}`);
+        }
+
+        const responseData = await response.json();
+
+        const sortedData = {
+            city: responseData.data.city,
+            country: responseData.data.country,
+            iconId: responseData.data.current.weather.ic,
+            temperature: responseData.data.current.weather.tp,
+        };
+
+        populateUI(sortedData);
+    } catch (error) {
+        loader.classList.remove('active');
+        errorInformation.textContent = error.message;
     }
-
-    const responseData = await response.json();
-    
-    const sortedData = {
-      city: responseData.data.city,
-      country: responseData.data.country,
-      iconId: responseData.data.current.weather.ic,
-      temperature: responseData.data.current.weather.tp,
-    }
-
-    populateUI(sortedData)
-  }
-  catch (error) {
-    loader.classList.remove("active");
-    errorInformation.textContent = error.message;
-  }
 }
-getWeatherData()
+getWeatherData();
 
+const temperature = document.querySelector('.temperature');
+const infoIcon = document.querySelector('.info-icon');
 
-const temperature = document.querySelector(".temperature");
-const infoIcon = document.querySelector(".info-icon");
-
-function populateUI(data){
-  temperature.textContent = `${data.temperature}°`;
-  infoIcon.src = `ressources/icons/${data.iconId}.svg`;
-  infoIcon.style.width = "110px";
-  loader.classList.remove("active");
+function populateUI(data) {
+    temperature.textContent = `${data.temperature}°`;
+    infoIcon.src = `ressources/icons/${data.iconId}.svg`;
+    infoIcon.style.width = '110px';
+    loader.classList.remove('active');
 }
